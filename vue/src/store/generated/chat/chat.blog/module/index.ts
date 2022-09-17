7b314163fbdd9c839f61d1fc9ea93283f7654abf
5iddy <siddhuyeturi@gmail.com>
2022-09-17 11:52:38 +0530

    scaffold post list

--- vue/src/store/generated/chat/chat.blog/module/index.ts
+++ vue/src/store/generated/chat/chat.blog/module/index.ts
@@ -4,9 +4,15 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgUpdatePost } from "./types/blog/tx";
+import { MsgDeletePost } from "./types/blog/tx";
+import { MsgCreatePost } from "./types/blog/tx";
 
 
 const types = [
+  ["/chat.blog.MsgUpdatePost", MsgUpdatePost],
+  ["/chat.blog.MsgDeletePost", MsgDeletePost],
+  ["/chat.blog.MsgCreatePost", MsgCreatePost],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -39,6 +45,9 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgUpdatePost: (data: MsgUpdatePost): EncodeObject => ({ typeUrl: "/chat.blog.MsgUpdatePost", value: MsgUpdatePost.fromPartial( data ) }),
+    msgDeletePost: (data: MsgDeletePost): EncodeObject => ({ typeUrl: "/chat.blog.MsgDeletePost", value: MsgDeletePost.fromPartial( data ) }),
+    msgCreatePost: (data: MsgCreatePost): EncodeObject => ({ typeUrl: "/chat.blog.MsgCreatePost", value: MsgCreatePost.fromPartial( data ) }),
     
   };
 };
